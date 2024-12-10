3e862d2800c2ac697c6a9e6c879c374878c27a4a
Marcella Fiori de Oliveira <marcellafiori23@gmail.com>
2024-12-10 10:57:40 -0300

    Finalização do desafio

---
+++ repet_txt.py
@@ -0,0 +1,6 @@
+#Solicitar uma string e um número inteiro como entrada. Depois, retornar a string repetindo no número de vezes informado.
+
+string = input("Digite uma string: ")
+numero = int(input("Digite um número: "))
+
+print((string + " ") * numero)
